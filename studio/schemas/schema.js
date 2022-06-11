757fcffb68267206bfe401f87f3679d0b444cbc3
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-11 22:16:20 +0400

    June 11, 2022 10:16 PM

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -11,6 +11,5 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
   ]),
 })
