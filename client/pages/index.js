024c661337ab496c695160158d23ba3bbc6341cc
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-12 01:46:43 +0400

    June 12, 2022 1:46 AM

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,12 @@
 import styles from "../styles/Home.module.css";
+import Sidebar from "../components/Sidebar.js"
 
 export default function Home() {
   return (
     <div className={styles.wrapper}>
+      <Sidebar />
       <div className={styles.main}>
-        
+
       </div>
     </div>
   );
