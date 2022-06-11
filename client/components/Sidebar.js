51c48e32e92939cf6db5c76d73cb12f050c2581a
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-12 01:49:41 +0400

    June 12, 2022 1:49 AM

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,4 +1,7 @@
-import React from 'react'
+// import React from 'react'
+import styles from '../styles/sidebar.module.css'
+import { useEffect, useState} from 'react'
+import { useRouter } from 'next/router'
 
 const Sidebar = () => {
     return <div>Sidebar</div>
