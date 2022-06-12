1163932f52eb41d76b34d887fc4dd9c5a7759ed2
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-12 23:42:03 +0400

    June 12, 2022 11:42 PM

--- client/components/dmCard.js
+++ client/components/dmCard.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Image from "next/image";
+import { useRouter } from "next/router";
+import styles from "../styles/dmCard.module.css";
 
 const dmCard = () => {
+
+    const changeUrl = () => {
+        router.push(`?channel=${id}&name=${name}`);
+      };
+
+
   return (
-    <div>dmCard</div>
+    <div className={styles.dmCard} onClick={changeUrl}>
+              <div className={styles.dmAvatarContainer}>
+                </div>
+                </div>
   )
 }
 
