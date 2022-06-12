77daa1b2b667c7b2ff945dd46dadd7f97c4092cf
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-12 20:03:09 +0400

    June 12, 2022 8:03 PM

--- client/components/conversationList.js
+++ client/components/conversationList.js
@@ -1,12 +1,16 @@
 import styles from '../styles/conversationList.module.css'
 import { useEffect, useState } from 'react'
 import Image from "next/image";
-
+import friends from "../assets/icons/friends.svg"
+import nitro from "../assets/icons/nitro.svg"
 
 
 const conversationList = () => {
   return (
-    <div>conversationList</div>
+    <div className={styles.conversationList}>
+    <div className={styles.conversationListTop}>
+    </div>
+    </div>
   )
 }
 
