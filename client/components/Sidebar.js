8d1eca1851d629ac8df276e36b3cf14fdeb64536
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-12 10:53:40 +0400

    June 12, 2022 10:53 AM

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -5,10 +5,10 @@ import { useRouter } from 'next/router'
 import RoomAvatar from './RoomAvatar'
 
 
-import avatar1 from '../assets/avatar1.png'
-import avatar2 from '../assets/avatar2.png'
-import avatar3 from '../assets/avatar3.png'
-import avatar4 from '../assets/avatar4.png'
+import avatar1 from '../assets/avatar-1.png'
+import avatar2 from '../assets/avatar-2.png'
+import avatar3 from '../assets/avatar-3.png'
+import avatar4 from '../assets/avatar-4.png'
 
 const dummyChannels = [
     {
