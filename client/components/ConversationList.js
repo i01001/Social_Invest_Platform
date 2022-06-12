90a9b99eedeaf13ff5527a7dcd42b082c5e37af7
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-12 20:25:39 +0400

    June 12, 2022 8:25 PM

--- client/components/ConversationList.js
+++ client/components/ConversationList.js
@@ -1,24 +1,41 @@
-import styles from '../styles/conversationList.module.css'
-import { useEffect, useState } from 'react'
+import styles from "../styles/conversationList.module.css";
+import { useEffect, useState } from "react";
 import Image from "next/image";
-import friends from "../assets/icons/friends.svg"
-import nitro from "../assets/icons/nitro.svg"
-
+import friends from "../assets/icons/friends.svg";
+import nitro from "../assets/icons/nitro.svg";
 
 const ConversationList = () => {
   return (
     <div className={styles.conversations}>
-    <div className={styles.conversationListTop}>
-        <input type='search' placeholder='Find or start a conversation' />
-    <div className={styles.conversationsContainer}>
-    <div clasName={styles.elementsContainer}>
-      <div className={styles.svgContainer}>
+      <div className={styles.conversationListTop}>
+        <input type="search" placeholder="Find or start a conversation" />
+        <div className={styles.conversationsContainer}>
+          <div clasName={styles.elementsContainer}>
+            <div className={styles.svgContainer}>
+              <Image
+                height={25}
+                width={25}
+                src={friends}
+                className={styles.svg}
+                alt="friends"
+              />
+            </div>
+          </div>
+          <div clasName={styles.elementsContainer}>
+            <div className={styles.svgContainer}>
+              <Image
+                height={25}
+                width={25}
+                src={nitro}
+                className={styles.svg}
+                alt="nitro"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      </div>
     </div>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
-export default ConversationList
+export default ConversationList;
