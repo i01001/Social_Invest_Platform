b5199cc15764df7c8c9f72b9de19d44036802f88
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-12 12:29:59 +0400

    June 12, 2022 12:29 PM

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,9 +3,9 @@ import Sidebar from "../components/Sidebar"
 
 export default function Home() {
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper}>    iiasdiasdiasndsadns                  Side bar 
       <Sidebar />
-      <div className={styles.main}>Conversation List View</div>
+      <div className={styles.main}>Conversation Chat List View</div>
     </div>
   );
 }
