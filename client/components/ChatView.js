4dc7e5353b4abea344dc6c6e757fc10675db31f3
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-13 01:36:14 +0400

    June 13, 2022 1:36 AM

--- client/components/ChatView.js
+++ client/components/ChatView.js
@@ -6,7 +6,13 @@ const ChatView = () => {
   return (
     <div className={styles.chatView}>
     <div className={styles.roomNameContainer}>
-        <Image height={20} width={20} src={at} className={styles.svg} alt='' />
+        <Image 
+        height={20} 
+        width={20} 
+        src={at} 
+        className={styles.svg} 
+        alt='' 
+        />
         <h3 className={styles.title}>Room Name</h3>
     </div>
     </div>
