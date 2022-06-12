0c861f930e0c6961f3d15e775ec9354c14025aab
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-13 00:03:51 +0400

    June 13, 2022 12:03 AM

--- client/components/dmCard.js
+++ client/components/dmCard.js
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import styles from "../styles/dmCard.module.css";
 
 const dmCard = (name, status, avatar, id) => {
+  const router = useRouter();
 
-    const changeUrl = () => {
-      };
-
+  const changeUrl = () => {};
 
   return (
     <div className={styles.dmCard} onClick={changeUrl}>
-              <div className={styles.dmAvatarContainer}>
-                <Image
-                src={avatar}
-                className={styles.dmAvatar}
-                height={48}
-                width={48}
-                alt={name}
-                />
-                </div>
-                </div>
-  )
-}
+      <div className={styles.dmAvatarContainer}>
+        <Image
+          src={avatar}
+          className={styles.dmAvatar}
+          height={48}
+          width={48}
+          alt={name}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default dmCard
+export default dmCard;
