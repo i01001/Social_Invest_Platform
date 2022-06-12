1add61172ace2a4d6b52f1720f6a8143cacf3704
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-12 11:16:13 +0400

    June 12, 2022 11:16 AM

--- client/components/RoomAvatar.js
+++ client/components/RoomAvatar.js
@@ -1,29 +1,29 @@
-import Image from 'next/image'
-import { useRouter } from 'next/router'
-import styles from '../styles/RoomAvatar.module.css'
+import Image from "next/image";
+import { useRouter } from "next/router";
+import styles from "../styles/RoomAvatar.module.css";
 
-import React from 'react'
+import React from "react";
 
-const RoomAvatar = ({id, avatar, name}) => {
-    const router = useRouter()
+const RoomAvatar = ({ id, avatar, name }) => {
+  const router = useRouter();
 
-    const changeUrl = () => {
-        router.push(`?channel=${id}&name=${name}`)
-    }
+  const changeUrl = () => {
+    router.push(`?channel=${id}&name=${name}`);
+  };
 
-    return (
+  return (
     <div className={styles.wrapper} onClick={changeUrl}>
-        <div className={styles.roomAvatar}>
-            <Image 
-                src={avatar}
-                className={styles.roomAvatarImage}
-                height={48}
-                width={48}
-                alt={name} 
-                />
-        </div>
+      <div className={styles.roomAvatar}>
+        <Image
+          src={avatar}
+          className={styles.roomAvatarImage}
+          height={48}
+          width={48}
+          alt={name}
+        />
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default RoomAvatar
+export default RoomAvatar;
--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,52 +1,52 @@
 // import React from 'react'
-import styles from '../styles/sidebar.module.css'
-import { useEffect, useState} from 'react'
-import { useRouter } from 'next/router'
-import RoomAvatar from './RoomAvatar'
+import styles from "../styles/sidebar.module.css";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import RoomAvatar from "./RoomAvatar";
 
-
-import avatar1 from '../assets/avatar-1.png'
-import avatar2 from '../assets/avatar-2.png'
-import avatar3 from '../assets/avatar-3.png'
-import avatar4 from '../assets/avatar-4.png'
+import avatar1 from "../assets/avatar-1.png";
+import avatar2 from "../assets/avatar-2.png";
+import avatar3 from "../assets/avatar-3.png";
+import avatar4 from "../assets/avatar-4.png";
 
 const dummyChannels = [
-    {
-        id: 1,
-        name: 'ethereum',
-        avatar: avatar1,
-    },
-    {
-        id: 2,
-        name: 'link',
-        avatar: avatar2,
-    },
-    {
-        id: 3,
-        name: 'graph',
-        avatar: avatar3,
-    },
-    {
-        id: 4,
-        name: 'dai',
-        avatar: avatar4,
-    },
-]
-
+  {
+    id: 1,
+    name: "ethereum",
+    avatar: avatar1,
+  },
+  {
+    id: 2,
+    name: "link",
+    avatar: avatar2,
+  },
+  {
+    id: 3,
+    name: "graph",
+    avatar: avatar3,
+  },
+  {
+    id: 4,
+    name: "dai",
+    avatar: avatar4,
+  },
+];
 
 const Sidebar = () => {
-    const router = useRouter() 
-    const [channels, setchannels] = useState([dummyChannels])
-    return <div className={styles.wrapper}>
-        {channels.map((channel, index) => (
-            <RoomAvatar
-            key={index}
-            id={channel.roomId}
-            avatar={channel.avatar}
-            name={channel.roomName}
-            />
-        ))}
-        </div>
-}
+  const router = useRouter();
+  const [channels, setChannels] = useState([dummyChannels]);
+  return (
+    <div className={styles.wrapper}>
+      {channels.map((channel, index) => (
+        <RoomAvatar
+          key={index}
+          id={channel.roomId}
+          avatar={channel.avatar}
+          name={channel.roomName}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
