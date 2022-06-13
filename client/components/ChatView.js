a777644fc0f41f2a2330f5d17490b2170b840368
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-13 10:26:45 +0400

    June 13, 2022 10:26 AM

--- client/components/ChatView.js
+++ client/components/ChatView.js
@@ -2,22 +2,21 @@ import React from 'react'
 import ChatHeader from './ChatHeader'
 import styles from "../styles/chatView.module.css"
 import Image from "next/image";
-import at from '../assets/icons/at.svg'
-
+ 
 
 const ChatView = () => {
   return (
     <div className={styles.chatView}>
         <ChatHeader />
     <div className={styles.messagesContainer}>
-        <Image 
+        {/* <Image 
         height={20} 
         width={20} 
         src={at} 
         className={styles.svg} 
         alt='' 
         />
-        <h3 className={styles.title}>Room Name</h3>
+        <h3 className={styles.title}>Room Name</h3> */}
     </div>
     </div>
   )
