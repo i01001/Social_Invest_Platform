14f6f4d6ca1f07a086683aba76f4e8481ebfe7e4
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-14 01:49:51 +0400

    June 14, 2022 1:49 AM

--- client/components/ConversationList.js
+++ client/components/ConversationList.js
@@ -6,11 +6,6 @@ import friends from "../assets/icons/friends.svg";
 import nitro from "../assets/icons/nitro.svg";
 import DmCard from "./DmCard";
 
-import avatar1 from "../assets/avatar-1.png";
-import avatar2 from "../assets/avatar-2.png";
-import avatar3 from "../assets/avatar-3.png";
-import avatar4 from "../assets/avatar-4.png";
-
 
 const ConversationList = () => {
   const router = useRouter();
