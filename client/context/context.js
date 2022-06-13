b8c9b4af3ac17fb1d9b10e8e0ccbb0ccf1e8f9c0
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-13 17:47:10 +0400

    June 13, 2022 5:47 PM

--- client/context/context.js
+++ client/context/context.js
@@ -6,3 +6,16 @@ export const SocialContext = createContext()
 
 const gun = Gun(['https://social-invest-platform.herokuapp.com/'])
 
+const reducer = (state, action) => {
+    try{
+        if(action.type == 'clear') return {messages: []}
+        if(action.type == 'add') return {messages: [...state.messages, action.data]}
+    } catch(error){console.log(error)}
+}
+
+export const SocialProvider = ({children }) => {
+
+    return (
+        <SocialContext.Provider value={{}}>{children}</SocialContext.Provider>
+    )
+}
