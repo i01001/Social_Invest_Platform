71dc673102ff421938299bac588b765efe4e732e
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-14 00:26:57 +0400

    June 14, 2022 12:26 AM

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -8,6 +8,7 @@ import avatar1 from "../assets/avatar-1.png";
 import avatar2 from "../assets/avatar-2.png";
 import avatar3 from "../assets/avatar-3.png";
 import avatar4 from "../assets/avatar-4.png";
+import { set } from "@project-serum/anchor/dist/cjs/utils/features";
 
 const dummyChannels = [
   {
@@ -35,6 +36,22 @@ const dummyChannels = [
 const Sidebar = () => {
   const router = useRouter();
   const [channels, setChannels] = useState(dummyChannels);
+
+  useEffect(async() => {
+    try{
+      const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/getchannels`,
+      )
+
+      const data = await response.json()
+      setChannels(data)
+      
+      router.push(`?channel=${data[0].roomId}&name=${data[0].roomName}`)
+
+    }catch(error) {}
+
+  },[])
+
   return (
     <div className={styles.wrapper}>
       {channels.map((channel, index) => (
