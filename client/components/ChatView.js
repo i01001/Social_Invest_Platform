377652ae4bbfbc6c6db6d951e78ac7bfb7c95130
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-13 10:32:31 +0400

    June 13, 2022 10:32 AM

--- client/components/ChatView.js
+++ client/components/ChatView.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ChatHeader from './ChatHeader'
 import styles from "../styles/chatView.module.css"
 import Image from "next/image";
+import MessageForm from "./MessageForm"
  
 
 const ChatView = () => {
@@ -9,14 +10,7 @@ const ChatView = () => {
     <div className={styles.chatView}>
         <ChatHeader />
     <div className={styles.messagesContainer}>
-        {/* <Image 
-        height={20} 
-        width={20} 
-        src={at} 
-        className={styles.svg} 
-        alt='' 
-        />
-        <h3 className={styles.title}>Room Name</h3> */}
+        <MessageForm />
     </div>
     </div>
   )
