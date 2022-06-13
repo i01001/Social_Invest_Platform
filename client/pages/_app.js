c0f908f858820dec3177f8b5c2f4ddd1bf61cb96
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-13 19:56:37 +0400

    June 13, 2022 7:56 PM

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import {SocialProvider} from '../context/context'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
