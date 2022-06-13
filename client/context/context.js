156791acb3c4c93b80a8caebfe80c4879b47c798
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-13 17:35:11 +0400

    June 13, 2022 5:35 PM

--- client/context/context.js
+++ client/context/context.js
@@ -1,3 +1,7 @@
 import { createContext, useState, useEffect, useReducer } from "react";
 import { useRouter} from "next/router";
 import Gun from 'gun';
+
+export const SocialContext = createContext()
+
+(['https://social-invest-platform.herokuapp.com/'])
