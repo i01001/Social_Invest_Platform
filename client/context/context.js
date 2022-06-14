1b1aa51e5c0ceb656a7fe0b639d1a8c93cb9b0b5
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-14 12:35:22 +0400

    June 14, 2022 12:35 PM

--- client/context/context.js
+++ client/context/context.js
@@ -1,104 +1,271 @@
-import { createContext, useState, useEffect, useReducer } from "react";
-import { useRouter } from "next/router";
-import Gun from "gun";
+// import { createContext, useState, useEffect, useReducer } from "react";
+// import { useRouter } from "next/router";
+// import Gun from "gun";
 
-export const SocialContext = createContext();
+// export const SocialContext = createContext();
 
-const gun = Gun(["https://social-invest-platform.herokuapp.com/"]);
+// const gun = Gun(["https://social-invest-platform.herokuapp.com/"]);
 
-const initialState = { messages: []}
+// const initialState = { messages: []}
+
+// const reducer = (state, action) => {
+//   try {
+//     if (action.type == "clear") return { messages: [] };
+//     if (action.type == "add")
+//       return { messages: [...state.messages, action.data] };
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// export const SocialProvider = ({ children }) => {
+//   const router = useRouter();
+//   const [state, dispatch] = useReducer(reducer, initialState);
+//   const [currentAccount, setCurrentAccount] = useState("");
+//   const [roomName, setRoomName] = useState("");
+//   const [placeholder, setPlaceholder] = useState("Message...");
+//   const [messageText, setMessageText] = useState("");
+//   const [currentUser, setCurrentUser] = useState();
+//   const [currentUserId, setCurrentUserId] = useState("");
+
+//   useEffect(() => {
+//     checkIfWalletIsConnected();
+//   }, []);
+
+//   const createUserAccount = async (userAddress=currentAccount) => {
+//     if (!window.ethereum) return;
+
+//     try {
+//       const data = {
+//         userAddress: userAddress,
+//       };
+
+//       try {
+//         await fetch(`${process.env.NEXT_PUBLIC_API_URL}/createuser`, {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify(data),
+//         });
+//       } catch (error) {
+//         console.error(error);
+//       }
+
+//       try {
+//         await fetch(`${process.env.NEXT_PUBLIC_API_URL}/createdm`, {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify(data),
+//         });
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   const checkIfWalletIsConnected = async () => {
+//     if (!window.ethereum) return;
+//     try {
+//       const addressArray = await window.ethereum.request({
+//         method: "eth_accounts",
+//       });
+//       if (addressArray.length > 0) {
+//         setCurrentAccount(addressArray[0]);
+//         createUserAccount(addressArray[0]);
+//       } else {
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   const connectWallet = async () => {
+//     if (!window.ethereum) return;
+//     try {
+//       const addressArray = await window.ethereum.request({
+//         method: "eth_requestAccounts",
+//       });
+//       if (addressArray.length > 0) {
+//         setCurrentAccount(addressArray[0]);
+//         createUserAccount(addressArray[0]);
+//       } else {
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   return (
+//     <SocialContext.Provider
+//       value={{
+//         currentAccount,
+//         roomName,
+//         setRoomName,
+//         placeholder,
+//         messageText,
+//         state,
+//         gun,
+//         connectWallet,
+//         currentUser,
+//       }}
+//     >
+//       {children}
+//     </SocialContext.Provider>
+//   );
+// };
+
+import { createContext, useState, useEffect, useReducer } from 'react'
+import { useRouter } from 'next/router'
+import Gun from 'gun'
+
+export const SocialContext = createContext()
+
+const gun = Gun(['https://discord-gun-node.herokuapp.com/gun'])
+
+const initialState = { messages: [] }
 
 const reducer = (state, action) => {
   try {
-    if (action.type == "clear") return { messages: [] };
-    if (action.type == "add")
-      return { messages: [...state.messages, action.data] };
+    if (action.type == 'clear') return { messages: [] }
+    if (action.type == 'add')
+      return { messages: [...state.messages, action.data] }
   } catch (error) {
-    console.log(error);
+    console.error(error)
   }
-};
+}
 
 export const SocialProvider = ({ children }) => {
-  const router = useRouter();
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const [currentAccount, setCurrentAccount] = useState("");
-  const [roomName, setRoomName] = useState("");
-  const [placeholder, setPlaceholder] = useState("Message...");
-  const [messageText, setMessageText] = useState("");
-  const [currentUser, setCurrentUser] = useState();
-  const [currentUserId, setCurrentUserId] = useState("");
+  const router = useRouter()
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const [currentAccount, setCurrentAccount] = useState('')
+  const [roomName, setRoomName] = useState('')
+  const [placeholder, setPlaceholder] = useState('Message...')
+  const [messageText, setMessageText] = useState('')
+  const [currentUser, setCurrentUser] = useState()
 
   useEffect(() => {
-    checkIfWalletIsConnected();
-  }, []);
+    checkIfWalletIsConnected()
+  }, [])
 
-  const createUserAccount = async (userAddress=currentAccount) => {
-    if (!window.ethereum) return;
+  useEffect( () => {
+    useEffectTwo()
+  }, [currentAccount])
+
+  const useEffectTwo = async () => {
+    if (!currentAccount) return
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/getCurrentUserData?account=${currentAccount}`,
+      )
+
+      const data = await response.json()
+      setCurrentUser(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+
+  useEffect(() => {
+    setRoomName(router.query.name)
+    dispatch({ type: 'clear', data: {} })
+    setPlaceholder(`Message ${router.query.name}`)
+    setMessageText('')
+    getMessages()
+  }, [router.query])
+
+  const getMessages = () => {
+    const _name = router.query.name
+    const _roomId = router.query.id
+    const messagesRef = gun.get(_name)
+
+    messagesRef.map().once(message => {
+      dispatch({
+        type: 'add',
+        data: {
+          sender: message.sender,
+          content: message.content,
+          avatar: message.avatar,
+          createdAt: message.createdAt,
+          messageId: message.messageId,
+        },
+      })
+    })
+  }
+
+  const createUserAccount = async (userAddress = currentAccount) => {
+    if (!window.ethereum) return
 
     try {
       const data = {
         userAddress: userAddress,
-      };
+      }
 
       try {
         await fetch(`${process.env.NEXT_PUBLIC_API_URL}/createuser`, {
-          method: "POST",
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify(data),
-        });
+        })
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
 
       try {
         await fetch(`${process.env.NEXT_PUBLIC_API_URL}/createdm`, {
-          method: "POST",
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify(data),
-        });
+        })
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
     } catch (error) {
-      console.log(error);
+      console.error(error)
     }
-  };
+  }
 
   const checkIfWalletIsConnected = async () => {
-    if (!window.ethereum) return;
+    if (!window.ethereum) return
     try {
       const addressArray = await window.ethereum.request({
-        method: "eth_accounts",
-      });
+        method: 'eth_accounts',
+      })
       if (addressArray.length > 0) {
-        setCurrentAccount(addressArray[0]);
-        createUserAccount(addressArray[0]);
+        setCurrentAccount(addressArray[0])
+        createUserAccount(addressArray[0])
       } else {
       }
     } catch (error) {
-      console.log(error);
+      console.error(error)
     }
-  };
+  }
 
   const connectWallet = async () => {
-    if (!window.ethereum) return;
+    if (!window.ethereum) return
     try {
       const addressArray = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
+        method: 'eth_requestAccounts',
+      })
+
       if (addressArray.length > 0) {
-        setCurrentAccount(addressArray[0]);
-        createUserAccount(addressArray[0]);
-      } else {
+        setCurrentAccount(addressArray[0])
+        createUserAccount(addressArray[0])
       }
     } catch (error) {
-      console.log(error);
+      console.error(error)
     }
-  };
+  }
 
   return (
     <SocialContext.Provider
@@ -108,6 +275,7 @@ export const SocialProvider = ({ children }) => {
         setRoomName,
         placeholder,
         messageText,
+        setMessageText,
         state,
         gun,
         connectWallet,
@@ -116,5 +284,5 @@ export const SocialProvider = ({ children }) => {
     >
       {children}
     </SocialContext.Provider>
-  );
-};
+  )
+}
