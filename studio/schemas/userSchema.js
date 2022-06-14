44c894bc3e710fa743330eeeab06324772cf5b31
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-14 10:55:27 +0400

    June 14, 2022 10:55 AM

--- studio/schemas/userSchema.js
+++ studio/schemas/userSchema.js
@@ -3,20 +3,20 @@ export const userSchema = {
     type: 'document',
     title: 'Users',
     fields: [
-        {
-            name: 'name',
-            type: 'string',
-            title: 'Name',
-        },
-        {
-            name: 'walletAddress',
-            type: 'string',
-            title: 'Wallet Address',
-        },
-        {
-            name: 'profileImage',
-            type: 'image',
-            title: 'Profile Image',
-        },
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Name',
+      },
+      {
+        name: 'walletAddress',
+        type: 'string',
+        title: 'Wallet Address',
+      },
+      {
+        name: 'profileImage',
+        type: 'image',
+        title: 'Profile Image',
+      },
     ],
-}
+  }
