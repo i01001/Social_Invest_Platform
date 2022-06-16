243b4637103cf13c4c1b22c10726757d307e3522
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-16 13:27:07 +0400

    June 16, 2022 1:27 PM

--- client/components/ConversationList.js
+++ client/components/ConversationList.js
@@ -12,6 +12,8 @@ import DailyIframe from '@daily-co/daily-js';
 import { css } from '@emotion/react'
 import Modal from 'react-modal'
 import { MoonLoader } from 'react-spinners'
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 
 Modal.setAppElement('#__next')
@@ -175,7 +177,7 @@ const ConversationList = () => {
 const popQuote = async () => {
   // <div className={styles.popListTop}>
   setstateN("asakkas");
-  
+
 
             <div className={styles.conversations}>
             <div className={styles.elementsContainer}>
@@ -244,8 +246,35 @@ const popQuote = async () => {
 
           <Modal isOpen={true} style={customStyles}>
                 <div className={style.wrapper}>
-      <div className={style.title}>Transaction in progress...</div>
-      <MoonLoader color={'#fff'} loading={true} css={cssOverride} size={50} />
+                <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80})} />
+      <input type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100})} />
+      <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+      <input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} />
+      <select {...register("Title", { required: true })}>
+        <option value="Mr">Mr</option>
+        <option value="Mrs">Mrs</option>
+        <option value="Miss">Miss</option>
+        <option value="Dr">Dr</option>
+      </select>
+
+      <input {...register("Developer", { required: true })} type="radio" value="Yes" />
+      <input {...register("Developer", { required: true })} type="radio" value="No" />
+
+      <input type="submit" />
+    </form>
+
+{/* export default function App() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+  
+  return (
+
+  );
+} */}
+      {/* <div className={style.title}>Transaction in progress...</div> */}
+      {/* <MoonLoader color={'#fff'} loading={true} css={cssOverride} size={50} /> */}
     </div>
         </Modal>
         </div>
