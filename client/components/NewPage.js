e89d4a1e1410b916e5d47754152fe0bf8bcb501d
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-17 19:16:36 +0400

    June 17, 2022 7:16 PM

--- client/components/NewPage.js
+++ client/components/NewPage.js
@@ -117,18 +117,20 @@ const enterPress = async () => {
   const quantValue = getValues("QuantityToken");      
 console.log(await fromTok, ToTok, quantValue);
 
-try {
-  const quote = await axios.get(
-    `https://api.1inch.io/v4.0/250/quote?fromTokenAddress=${fromTok}&toTokenAddress=${ToTok}&amount=${quantValue}`
-  );
-  console.log(quote);
-  if (quote) {
-    const name1 = await quote.data.toTokenAmount;
-    console.log(await name1);
-  }
-} catch (error) { 
-  console.error("Quote execution error", error);
-}
+const quoteYes = QuoteorSwap.isChecked;
+
+// try {
+//   const quote = await axios.get(
+//     `https://api.1inch.io/v4.0/250/quote?fromTokenAddress=${fromTok}&toTokenAddress=${ToTok}&amount=${quantValue}`
+//   );
+//   console.log(quote);
+//   if (quote) {
+//     const name1 = await quote.data.toTokenAmount;
+//     console.log(await name1);
+//   }
+// } catch (error) { 
+//   console.error("Quote execution error", error);
+// }
 }
 
 
