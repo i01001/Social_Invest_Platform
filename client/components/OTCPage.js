5dbddb573fe7e5cffe483f822219418c29542aa0
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-21 04:06:12 +0400

    June 21, 2022 4:06 AM

--- client/components/OTCPage.js
+++ client/components/OTCPage.js
@@ -328,8 +328,8 @@ const OTCPage = () => {
 
   const enterPress = async () => {
     const ACDMContract = "0xE3cf96b99c06eADC26E92ac3a01dac64E9f5bF68";
-    const quantityT = 5000000000;
-    const maticAmount = 100000000;
+    const quantityT = 3000000000;
+    const maticAmount = 1000000000;
     console.log("contract", myContract);
     console.log("abi", contractABI);
     console.log("contract address", contractAddress);
